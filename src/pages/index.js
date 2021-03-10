b6ce5cb8f529d5cc3836fd207deff0acd929dc8a
Audrey Wong <audreydotwong@gmail.com>
2021-03-10 14:59:27 -0500

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+    return (<div class="content">
+        <h1>Hello Netlify! 🖖</h1>
+        <p>I'm Audrey Wong.</p>
+        <p>I'd like very much to join Netlify's Support Engineer team.</p>
+        <p>I love coding, painting, nature, animals, and I really love squirrels! 🐿️</p>
+    </div>);
 }
